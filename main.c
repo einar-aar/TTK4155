f175b236ffd1c23d556c93c0adcf1584a5a1a374
monarogg <monanr@stud.ntnu.no>
2025-09-02 14:27:32 +0200

    test program

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+
+
+// ATmega162 cpu frequency
+#define F_CPU 4915200UL
+#include <util/delay.h>
+
+
+int main(void) {
+
+    DDRA = (1 << PA0);
+
+    for (int i = 0; i < 10; i++) {
+
+        PORTA ^= (1 << PA0);
+        _delay_ms(1000);
+
+    }
+    return 0;
+}
