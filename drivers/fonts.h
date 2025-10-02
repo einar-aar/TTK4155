9d4127aca6d33d1272125231d6355b5275704c70
pederingmar <piddi056@gmail.com>
2025-10-02 10:37:48 +0200

    OLED print func works

---
+++ drivers/fonts.h
@@ -0,0 +1,18 @@
+/*
+ * fonts.h
+ *
+ * Library of fonts
+ * Large: 8x8, normal: 5x7 and small: 4x7
+ */
+#ifndef FONTS_H_
+#define FONTS_H_
+#define FONTS_H_FIRST 32
+#define FONTS_H_LAST 127
+
+extern const unsigned char font8[95][8];
+extern const unsigned char font4[95][4];
+extern const unsigned char font5[95][5];
+
+
+
+#endif /* FONTS_H_ */
