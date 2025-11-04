fc20b7c6ea4a8096f05d68652d7438a864d022d5
einar-aar <einar@skogn.no>
2025-11-04 08:43:21 +0100

    update

--- ARM-cortex-M3_node2/build/drivers/PWM.d
+++ ARM-cortex-M3_node2/build/drivers/PWM.d
@@ -72,4 +72,5 @@ build/drivers/PWM.o: drivers/PWM.c sam/sam3x/include/sam.h \
  sam/sam3x/include/instance/instance_wdt.h \
  sam/sam3x/include/instance/instance_rtc.h \
  sam/sam3x/include/instance/instance_gpbr.h \
- sam/sam3x/include/pio/pio_sam3x8e.h drivers/PWM.h
+ sam/sam3x/include/pio/pio_sam3x8e.h drivers/PWM.h \
+ drivers/can_controller.h
--- ARM-cortex-M3_node2/drivers/PWM.h
+++ ARM-cortex-M3_node2/drivers/PWM.h
@@ -1,3 +1,5 @@
 
 
 void PWMinit(uint32_t mck);
+int controller_output_to_duty_cycle(int contr_output);
+void set_duty_cycle(int duty_cycle);
