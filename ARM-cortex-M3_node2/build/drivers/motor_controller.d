7b014ff7881a52d75342f2a18390725a5c54b7ea
einar-aar <einar@skogn.no>
2025-11-06 15:30:09 +0100

    checkpoint

--- ARM-cortex-M3_node2/build/drivers/motor_controller.d
+++ ARM-cortex-M3_node2/build/drivers/motor_controller.d
@@ -1,5 +1,6 @@
 build/drivers/motor_controller.o: drivers/motor_controller.c \
- sam/sam3x/include/sam.h sam/sam3x/include/sam3x8e.h sam/cmsis/core_cm3.h \
+ drivers/motor_controller.h sam/sam3x/include/sam.h \
+ sam/sam3x/include/sam3x8e.h sam/cmsis/core_cm3.h \
  sam/cmsis/cmsis_version.h sam/cmsis/cmsis_compiler.h \
  sam/cmsis/cmsis_gcc.h sam/cmsis/mpu_armv7.h \
  sam/sam3x/source/system_sam3x.h sam/compiler.h sam/parts.h \
@@ -72,4 +73,5 @@ build/drivers/motor_controller.o: drivers/motor_controller.c \
  sam/sam3x/include/instance/instance_wdt.h \
  sam/sam3x/include/instance/instance_rtc.h \
  sam/sam3x/include/instance/instance_gpbr.h \
- sam/sam3x/include/pio/pio_sam3x8e.h
+ sam/sam3x/include/pio/pio_sam3x8e.h drivers/PWM.h \
+ drivers/can_controller.h
